d2a9100e3646487db168e8b9c86a1e3b6a778944
Dushyant Rajput <dushyantr737@gmail.com>
2026-04-06 21:45:34 +0530

    fix: resolve record route + syntax error

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const authRouter = require("./routes/auth.route.js")
 const accountRouter = require("./routes/account.route.js")
-const transactionRoutes=require("./routes/transaction.route.js")
+const transactionRoutes = require("./routes/transaction.route.js")
+const Dashboard = require("./routes/dashboard.routes.js")
+const Record=require("./routes/record.route.js")
 const cookieParser=require("cookie-parser")
 const app = express()
 
@@ -19,11 +21,11 @@ app.use("/api/accounts", accountRouter)
 /**
  * - User Record Routes Api Call
  */
-app.use("/api/records", require("./routes/record.routes"));
+app.use("/api/records", Record);
 /**
  * - User Dashboard Routes Api Call
  */
-app.use("/api/dashboard", require("./routes/dashboard.routes"));
+app.use("/api/dashboard",Dashboard );
 
 app.use("/api/transaction", transactionRoutes)
 
